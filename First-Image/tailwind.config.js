6589a5c3a6153e844b47a951b1b61d9d7da2c16e
Kyrudia <eileena.patamia@epitech.eu>
2023-10-30 17:41:35 +0100

    [ADD] Project

--- First-Image/tailwind.config.js
+++ First-Image/tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./dist/*{html,js}"],
   theme: {
+    screens: {
+      'tel': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
 }
-
