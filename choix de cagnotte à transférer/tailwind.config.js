46e29b1092f2ea669e0904bc7751699f9c75dbc5
Kyrudia <eileena.patamia@epitech.eu>
2023-12-06 17:46:17 +0100

    [ADD] Choix de cagnotte

--- choix de cagnotte à transférer/tailwind.config.js
+++ choix de cagnotte à transférer/tailwind.config.js
@@ -3,12 +3,14 @@ module.exports = {
   content: ["./dist/*{html,js}"],
   theme: {
     screens: {
-      'tel': '425px',
-      'telL': '500px',
+      'tel': '320px',
+      'telL': '375px',
+      'telxl': '500px',
       'sm': '640px',
       'md': '768px',
       'lg': '1020px',
       'xl': '1280px',
+      '1xl': '1339px',
       '2xl': '1536px',
       '4k': '2560px',
     },
